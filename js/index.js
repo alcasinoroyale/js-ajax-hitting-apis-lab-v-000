ad73dcd9ebe68bd82ff6bae8a8d44e87a2de3b98
Alex Zdatny <alexzdatny@gmail.com>
2018-12-30 23:31:37 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function getRepositories() {
   const username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories);
-  req.open('GET', 'https://api.github.com/users/' + name + '/repos');
+  req.open('GET', 'https://api.github.com/users/' + username + '/repos');
   req.send();
 }
 
