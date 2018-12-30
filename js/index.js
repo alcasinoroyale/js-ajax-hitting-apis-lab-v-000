566a204aaa4438cd119dac2a12675e2db0b2cb44
Alex Zdatny <alexzdatny@gmail.com>
2018-12-30 23:24:30 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -34,9 +34,18 @@ function getCommits(el) {
   req.send();
 }
 
-
-
-
-
-  )}`
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+      '<li><strong>' +
+      commit.commit.author.name +
+      commmit.author.login +
+      '</strong> - ' +
+      commit.commit.message +
+      '</li'
+    )
+    .join('')}</ul>`;
+    document.getElementById('commits').innerHTML = commitsList;
 }
