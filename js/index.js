8208404b4fe372889c5fe3341bc713abd562a1f7
Alex Zdatny <alexzdatny@gmail.com>
2018-12-30 23:10:33 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,3 +6,29 @@ function getRepositories() {
   req.open('GET', 'https://api.github.com/users/' + name + '/repos');
   req.send();
 }
+
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos.map(
+    r =>
+    '<li>' +
+    r.name +
+    '<br> https://github.com/'+ r.owner.login + '/' + r.name +
+		'<br><a href="#" data-username="'+ r.owner.login +
+    '"data-repository="' + r.name +
+    '"onclick="getCommits(this)">GetCommits</a>' +
+		'<br><a href="#" data-username="'+ r.owner.login +
+    '"data-repository="' + r.name +'"onclick="getBranches(this)">GetBranches</a></li>'
+		)
+    .join('')}</ul>`;
+	document.getElementById('repositories').innerHTML = repoList;
+}
+
+
+
+
+
+
+  )}`
+}
